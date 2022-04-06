94b0b7d7b46cd56e1f89a71dda296ca1bceb2f03
Urbana28 <ana.urbana28@gmail.com>
2022-04-06 14:52:30 +0300

    working with images behavior

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,12 +5,13 @@ const config: GatsbyConfig = {
     title: `new`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-netlify-cms", "gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
+  plugins: ["gatsby-plugin-netlify-cms", "gatsby-plugin-sass", "gatsby-plugin-image", 
+   "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
     options: {
       "icon": "src/images/icon.png"
     }
-  }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", 
+  }, "gatsby-plugin-mdx", 
   {
     resolve: 'gatsby-source-filesystem',
     options: {
@@ -18,20 +19,14 @@ const config: GatsbyConfig = {
       "path": "./src/images/"
     }
   }, 
+  "gatsby-plugin-sharp", "gatsby-transformer-sharp", 
   {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "pages",
       "path": "./src/pages/"
     }
-  }, 
-  // {
-  //   resolve: 'gatsby-source-filesystem',
-  //   options: {
-  //     "name": 'markdown-pages',
-  //     "path": `${__dirname}/blog`
-  //   },
-  // }, 
+  },  
   // {
   //   resolve: "gatsby-plugin-netlify-cms",
   //   options: {
--- src/templates/BlogPost.tsx
+++ src/templates/BlogPost.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import PropTypes from "prop-types";
 import { kebabCase } from "lodash";
 import { graphql, Link } from "gatsby";
-import { StaticImage } from "gatsby-plugin-image";
+import { GatsbyImage, getImage, IGatsbyImageData, StaticImage } from "gatsby-plugin-image";
+
 
 type BlogPostTemplateProps = {
   data: any
@@ -12,12 +13,41 @@ const BlogPostTemplate: React.FC<BlogPostTemplateProps>  = ({
   data // this prop will be injected by the GraphQL query we'll write in a bit
 }) => {
   const { markdownRemark: post } = data // data.markdownRemark holds your post data
+
+  const image = getImage(post.frontmatter.image);
+  console.log('POST_IMAGE', image)
+  const car_def = "https://images.unsplash.com/photo-1523828446771-151afb8374f1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=927&q=80";
+  
+  const imageStyle = { borderRadius: "5px" };
+
+  // const imagePreview = () => {
+  //   if(!!image && !!image.childImageSharp) {
+  //       <GatsbyImage
+  //         image={image.childImageSharp.gatsbyImageData}
+  //         style={imageStyle}
+  //         alt={alt}
+  //       />
+  //   } else if(!!childImageSharp) {
+  //       <GatsbyImage
+  //         image={childImageSharp.gatsbyImageData}
+  //         style={imageStyle}
+  //         alt={alt}
+  //       />
+  //       // for Netlify CMS 
+  //   } else if (image) {
+  //     return <img style={imageStyle} src={image} alt={alt} />;
+  //   } else {
+  //     return null
+  //   }
+  // };
   
   return (
     <div className="blog-post-container">
       <div className="blog-post">
         <h1>{post.frontmatter.title}</h1>
-        <StaticImage src={post.frontmatter.image} alt="Car" placeholder="blurred" layout="fixed" />
+        {/* {imagePreview()} */}
+        {/* <GatsbyImage image={image || image_data} alt="Car" /> */}
+        <StaticImage src={post.frontmatter.image} alt={""} />
         <div
           className="blog-post-content"
           dangerouslySetInnerHTML={{ __html: post.html }}
@@ -37,6 +67,7 @@ export const pageQuery = graphql`
         date(formatString: "MMMM DD, YYYY")
         path
         title
+        image
       }
     }
   }
