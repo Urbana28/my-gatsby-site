884d8e2a639ce40b808ba5d8cabb86d0ebef16c8
Urbana28 <ana.urbana28@gmail.com>
2022-05-17 23:39:31 +0300

    added dotenv config

--- prebuild.sh
+++ prebuild.sh
@@ -6,19 +6,18 @@ USER_SET_BRANCH=$1
 
 DETECTED_BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 BRANCH="${USER_SET_BRANCH:-$DETECTED_BRANCH}"
-
-echo ${GATSBY_PROD_API}
+MAIN="main"
 
 # Set branch on which the CMS edits files by creating
 # the netlify config from the netlify-cms-config and overwrite
 # the branch setting.
-if [$BRANCH == "main"]
-then
-  sed -i -e "s|_GATSBY_API|${GATSBY_PROD_API}|g" .env
+
+if [ "$BRANCH" = "$MAIN" ]; then
+    sed -i -e "s|_GATSBY_API|${GATSBY_PROD_API}|g" .env
 else
-  sed -i -e "s|_GATSBY_API|${GATSBY_QA_API}|g" .env  #путь до файла с урлами
+    sed -i -e "s|_GATSBY_API|${GATSBY_QA_API}|g" .env
 fi
 
 sed -i -e "s|GATSBY_CMS_BRANCH|$BRANCH|g" ./static/admin/config.yml
 
-cat netlify.toml
+
